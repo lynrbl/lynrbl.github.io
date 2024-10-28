98cf1ef8d698e298e7ccc8de39eb396a59fdfb16
lynrbl <114229444+lynrbl@users.noreply.github.com>
2024-10-28 14:56:39 +0100

    new

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,41 @@
-"use strict"
-const response = await fetch("https://restcountries.com/v3.1/all");
+"use strict";
 
-const pays = await response.json();
-let html = "";
-html += "<tr>"
-html += "<th>Name common</th>"
-html += "<th>Image</th>"
-html += "</tr>"
-pays.forEach(pays => {
-    html += "<tr>";
-    html += `<td> ${pays.name.common} </td>`;
-    html += `<td><img src="${pays.flags.png}" alt=""></td>`;
-    html += "</tr>";
+const map = L.map('map').setView([46.2044, 6.1432], 13); 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+}).addTo(map);
+
+
+map.on('click', (e) => {
+    const { lat, lng } = e.latlng;
+    const marker = L.marker([lat, lng]).addTo(map);
+
+    marker.on('click', () => {
+        showMarkerDetails(marker);
+    });
 });
-document.getElementById("tableau").innerHTML = html
-console.log(pays)
+
+function showMarkerDetails(marker) {
+    const details = `
+        <div class="w3-container">
+            <h3>Détails de l'Objet Caché</h3>
+            <p>Coordonnées : ${marker.getLatLng().toString()}</p>
+            <button onclick="backToMap()" class="w3-button w3-green">Retour à la carte</button>
+        </div>
+    `;
+    document.body.innerHTML = details;
+}
+
+function backToMap() {
+    location.reload();
+}
+async function openCamera() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const videoElement = document.getElementById('video');
+        videoElement.srcObject = stream;
+        videoElement.style.display = 'block'; 
+    } catch (err) {
+        console.error("Erreur d'accès à la caméra", err);
+    }
+}
