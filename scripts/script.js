30865ceced9ddd6840f2ae82309a686fd4163049
lynrbl <114229444+lynrbl@users.noreply.github.com>
2024-10-28 15:16:45 +0100

    dwad

--- scripts/script.js
+++ scripts/script.js
@@ -6,21 +6,25 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
+const markers = [
+    L.marker([43.2044, 6.432]).addTo(map),
+    L.marker([42.2044, 7.432]).addTo(map),
+    L.marker([47.2044, 6.132]).addTo(map),
+    L.marker([41.2044, 6.142]).addTo(map),
+    L.marker([46.2044, 6.143]).addTo(map),
+    L.marker([43.2044, 5.32]).addTo(map),
+    L.marker([49.2044, 6.2]).addTo(map),
+];
+
 map.on('click', (e) => {
     const { lat, lng } = e.latlng;
-    const marker = L.marker([lat, lng]).addTo(map);
+    const newMarker = L.marker([lat, lng]).addTo(map);
 
-    marker.on('click', () => {
-        showMarkerDetails(marker);
+    newMarker.on('click', () => {
+        showMarkerDetails(newMarker);
     });
 });
-const marker1 = L.marker([43.2044, 6.432]).addTo(map);
-const marker2 = L.marker([42.2044, 7.432]).addTo(map);
-const marker3 = L.marker([47.2044, 6.132]).addTo(map);
-const marker4 = L.marker([41.2044, 6.142]).addTo(map);
-const marker5 = L.marker([46.2044, 6.143]).addTo(map);
-const marker6 = L.marker([43.2044, 5.32]).addTo(map);
-const marker7 = L.marker([49.2044, 6.2]).addTo(map);
+
 function showMarkerDetails(marker) {
     const details = `
         <div class="w3-container">
@@ -35,6 +39,7 @@ function showMarkerDetails(marker) {
 function backToMap() {
     location.reload();
 }
+
 async function openCamera() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({ video: true });
