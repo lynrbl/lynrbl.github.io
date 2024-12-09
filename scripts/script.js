7c974a597816d55f337d870c9a962bb4fe357fce
lynrbl <114229444+lynrbl@users.noreply.github.com>
2024-12-09 15:23:55 +0100

    wdwa

--- scripts/script.js
+++ scripts/script.js
@@ -5,27 +5,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
 }).addTo(map);
 
-const markers = [
-    L.marker([46.236853, 6.126938]).addTo(map),
-    L.marker([46.19017, 6.114063]).addTo(map),
-    L.marker([46.190764, 6.151485]).addTo(map),
-    L.marker([46.181257, 6.102905]).addTo(map),
-    L.marker([46.2044, 6.143]).addTo(map),
-    L.marker([43.2044, 5.32]).addTo(map),
-    L.marker([49.2044, 6.2]).addTo(map)
-];
-
-markers.forEach(marker => {
-    marker.on('click', () => {
-        map.closePopup();
-        console.log("ouvre le popup");
-        marker.bindPopup(
-            `<div class="w3-container">
-                <h3>Détails de l'Objet Caché</h3>
-                <p>Coordonnées : ${marker.getLatLng().toString()}</p>
-            </div>`
-        ).openPopup();
-    });
+map.on('click', (e) => {
+    const { lat, lng } = e.latlng;
+    const newMarker = L.marker([lat, lng]).addTo(map)
+    .bindPopup(`<div class="w3-container"><h3>Détails de l'Objet Caché</h3> <p>nduwabudbwaobd</p> </div>`)
+    .openPopup();
+    
 });
 
 let currentStream;
